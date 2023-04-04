0ee0df31094521dd8026b15025bbd7b7f78f29c9
PedroPertusi <pedrovazpertusi@gmail.com>
2023-04-04 14:41:01 -0300

    A+ feito

--- pygame_test.py
+++ pygame_test.py
@@ -11,7 +11,7 @@ d = 1
 ang_x = 0
 ang_y = 0
 ang_z = 0
-
+girando = False
 
 # Velocidade angular (rotacoes por segundo)
 v = 0.2
@@ -28,6 +28,7 @@ COR_PONTOS = (200, 30, 20)
 
 rodando = True
 while rodando:
+    
 
     P = np.array([[1 ,0, 0, 0], [0, 1, 0, 0], [0, 0, 0, -d], [0, 0, -1/d, 0]])
 
@@ -37,16 +38,16 @@ while rodando:
     [0, 0, 0, 1]])
 
     ry = np.array([[math.cos(math.radians(ang_y)), 0, math.sin(math.radians(ang_y)), 0],
-                    [0, 1, 0, 0],
-                    [-math.sin(math.radians(ang_y)), 0, math.cos(math.radians(ang_y)), 0],
-                    [0, 0, 0, 1]])
+                [0, 1, 0, 0],
+                [-math.sin(math.radians(ang_y)), 0, math.cos(math.radians(ang_y)), 0],
+                [0, 0, 0, 1]])
 
     rz = np.array([[math.cos(math.radians(ang_z)), -math.sin(math.radians(ang_z)), 0, 0],
                 [math.sin(math.radians(ang_z)), math.cos(math.radians(ang_z)), 0, 0],
                 [0, 0, 1, 0],
                 [0, 0, 0, 1]])
     
-    proj = rz @ ry @ rz @ objeto
+    proj = rz @ ry @ rx @ objeto
     proj[2] = proj[2] + 4
     proj = P @ proj
     proj = proj/proj[3]
@@ -58,13 +59,29 @@ while rodando:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             rodando = False
+
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_x:
+            if event.key == pygame.K_w and not girando:
                 ang_x += 10
-            elif event.key == pygame.K_y:
+            elif event.key == pygame.K_s and not girando:
+                ang_x -= 10
+            elif event.key == pygame.K_a and not girando:
                 ang_y += 10
-            elif event.key == pygame.K_z:
+            elif event.key == pygame.K_d and not girando:
+                ang_y -= 10
+            elif event.key == pygame.K_z and not girando:
                 ang_z += 10
+            elif event.key == pygame.K_x and not girando:
+                ang_z -= 10
+            elif event.key == pygame.K_r:
+                ang_x = 0
+                ang_y = 0
+                ang_z = 0
+            elif event.key == pygame.K_g:
+                if girando:
+                    girando = False
+                else:
+                    girando = True
         elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 4:
             if d - 0.2 > 0:
                 d -= 0.2
@@ -72,6 +89,10 @@ while rodando:
             if d + 0.2 > 0:
                 d += 0.2
 
+    if girando:
+        ang_x += 1
+        ang_y += 1
+        ang_z += 1
 
     # Controlar frame rate
     clock.tick(FPS)
@@ -79,7 +100,7 @@ while rodando:
     screen.fill(BLACK)
 
     for i in range(8):
-        pygame.draw.circle(screen, COR_PERSONAGEM, (proj[0][i], proj[1][i]), 10)
+        pygame.draw.circle(screen, (0,150,0), (proj[0][i], proj[1][i]), 10)
 
     for start,end in [[0, 1], [1, 2], [2, 3], [3, 0], [4, 5], [5, 6], [6, 7], [7, 4], [0, 4], [1, 5], [2, 6], [3, 7]]:
         pygame.draw.line(screen, (0,255,0), (proj[0][start], proj[1][start]), (proj[0][end], proj[1][end]),10)
